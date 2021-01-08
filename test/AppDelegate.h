ddea14ff53d6fcf74f9b2d7600f758029da3aada
szc <shizhuochan@bytedance.com>
2021-01-08 11:36:44 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by bytedance on 2021/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by bytedance on 2021/1/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
